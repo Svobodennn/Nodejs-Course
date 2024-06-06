8d3880c524fc3b4b381a3aec9014b7635217e670
Svobodennn <melih.sarac@hotmail.com>
2024-06-06 22:36:54 +0300

    Commonjs Modules

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+console.log(`Random number: ${generateRandomNumber()}`)
+console.log(`26 celcius to fahrenheit: ${celciusToFahrenheit(26)}`)
