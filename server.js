930dcde23323931c49d6d03c811c55df73a24e2c
Svobodennn <melih.sarac@hotmail.com>
2024-06-06 22:56:22 +0300

    HTTP Module & Create Server

---
+++ server.js
@@ -0,0 +1,16 @@
+import http from 'http';
+const PORT = 5000;
+
+const server = http.createServer((req,res) =>{
+    // res.setHeader('Content-Type', 'text/html');
+    // res.statusCode = 404;
+    // res.write('<h1>Hello World!</h1>');
+    // res.end();
+
+    res.writeHead(500,{'Content-Type': 'application/json'})
+    res.end(JSON.stringify({ message: 'Server Error'}));
+});
+
+server.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`)
+});
