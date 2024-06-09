5efe5760f288b57e4834430248d6b07155cf08c2
Svobodennn <melih.sarac@hotmail.com>
2024-06-09 20:32:53 +0300

    req object & testing postman

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const PORT = process.env.PORT;
 
 const server = http.createServer((req,res) =>{
     // res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200,{'Content-Type': 'text/html'})
+
     // res.statusCode = 404;
-    // res.write('<h1>Hello World!</h1>');
+    res.write('<h1>Hello World!</h1>');
     // res.end();
 
-    res.writeHead(500,{'Content-Type': 'application/json'})
-    res.end(JSON.stringify({ message: 'Server Error'}));
+    console.log(req.url)
+    console.log(req.method)
+
+    // res.writeHead(500,{'Content-Type': 'application/json'})
+    // res.end(JSON.stringify({ message: 'Server Error'}));
 });
+
 
 server.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
