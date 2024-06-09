25bdbd2247c60cf1ff8d3f99f9f540110b4de1ff
Svobodennn <melih.sarac@hotmail.com>
2024-06-09 20:40:21 +0300

    Simple routing

--- server.js
+++ server.js
@@ -4,17 +4,42 @@ const PORT = process.env.PORT;
 
 const server = http.createServer((req,res) =>{
     // res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200,{'Content-Type': 'text/html'})
-
+    // res.writeHead(200,{'Content-Type': 'text/html'})
+// 
     // res.statusCode = 404;
-    res.write('<h1>Hello World!</h1>');
+    // res.write('<h1>Hello World!</h1>');
     // res.end();
-
-    console.log(req.url)
-    console.log(req.method)
+// 
+    // console.log(req.url)
+    // console.log(req.method)
 
     // res.writeHead(500,{'Content-Type': 'application/json'})
     // res.end(JSON.stringify({ message: 'Server Error'}));
+
+    try {
+        if(req.method === 'GET'){
+            if(req.url === '/'){
+                res.writeHead(200,{'Content-Type': 'text/html'})
+                res.write('<h1>Homepage</h1>');
+        
+            }
+            else if(req.url === '/about'){
+                res.writeHead(200,{'Content-Type': 'text/html'})
+                res.write('<h1>About</h1>');
+        
+            }
+            else{
+                res.writeHead(404,{'Content-Type': 'text/html'})
+                res.write('<h1>Not found</h1>');
+            }
+        }
+        else {
+            throw new Error('method not valid')
+        }
+    } catch (error) {
+        res.writeHead(500,{'Content-Type': 'text/html'})
+        res.write('Server error');
+    }
 });
 
 
